4ba2413b89f5162b672dcb3fd78f04b584647932
github username <github email address>
2019-06-10 15:26:56 -0500

    Begin User authorization work, create auth_controller and application_controller private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    # if session[:user_id].nil?
+    #   redirect_to :login
+    # end
+  end
+
+  def current_user
+    # @user ||= User.find(session[:user_id])
+  end
+
+  def secret_key
+    'secret'
+    # look into Rails application secret to generate secret
+  end
+
+  def encode(payload)
+    # payload = { user_id: User.find_by(email: email).id }
+    JWT.encode(payload, secret_key, 'HS256')
+    # => jaowenopvhGIBBERISH9q348htnt0gnb9neu0134u0
+  end
+
+  def decode(token)
+    JWT.decode(token, secret_key, true, { algorithm: 'HS256' })
+    # => [{"user_id"=>2}, {"alg"=>"HS256"}]
+  end
+
 end
---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,19 @@
+class AuthController < ApplicationController
+  skip_before_action :authenticate, :login
+
+  def login
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      # Rails version:
+      # session[:user_id] = user.id
+      token = encode({ user_id: user.id })
+      render json: { token: token, success: true }
+    else
+      # Rails version:
+      # indicate some kind of error
+      # keep errors generic for safety (not e-mail or password wrong, just something went wrong)
+      render json: { success: false }, status: 401
+    end
+  end
+
+end

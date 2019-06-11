13709d9b1e4ba5c65877ad596b977b509ff2b21e
github username <github email address>
2019-06-11 12:47:23 -0500

    Restful UsersController not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,27 @@ class UsersController < ApplicationController
     render json: { name: current_user.name, email: current_user.email }
   end
 
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     render json: @user
+  #   else
+  #     render json: { error: "Unable to create this user" }, status: 400
+  #   end
+  # end
+  #
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   if @user.destroy
+  #     status: 202
+  #   else
+  #     render json: { error: "Unable to delete this user" }, status: 400
+  #   end
+  # end
+  #
+  # private
+  #   def user_params
+  #     params.permit(:name, :email, :password)
+  #   end
+
 end

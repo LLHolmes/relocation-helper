da81a14802c9b370ef0593f37860a04b70d87b93
github username <github email address>
2019-06-12 08:45:27 -0500

    Clean up React default App & index.html files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import UserPage from './UserPage';
@@ -9,20 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <HomeSearchForm />
-          <Login />
-          <UserPage />
-        </header>
+        <HomeSearchForm />
+        <Login />
+        <UserPage />
       </div>
     );
   };

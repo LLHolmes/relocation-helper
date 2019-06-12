5eb2ae4a71192357c2a29865e85a0a973113c52c
github username <github email address>
2019-06-12 15:07:28 -0500

    Add signup form actions and reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,12 +4,14 @@ import user from './userReducer'
 import userHomes from './userHomesReducer'
 import userFavorites from './userFavoritesReducer'
 import formLogin from './formLoginReducer'
+import formSignup from './formSignupReducer'
 
 const rootReducer = combineReducers({
   search,
   user,
   userHomes,
   userFavorites,
+  formSignup,
   formLogin
 });
 

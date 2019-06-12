cec054c473cfa6cc79ed659f0b5e85f018d55bdc
github username <github email address>
2019-06-12 13:11:36 -0500

    Add componentDidMount to getCurrentUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getCurrentUser } from './actions/userActions.js'
 import './stylesheets/App.css';
 import Login from './components/Login';
 import UserPage from './components/UserPage';
 import HomeSearchForm from './components/HomeSearchForm';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  };
+
   render() {
     return (
       <div className="App">
@@ -16,4 +23,4 @@ class App extends Component {
   };
 };
 
-export default App;
+export default connect(null, { getCurrentUser })(App);

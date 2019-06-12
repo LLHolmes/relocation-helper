0604bc4b66b765ed8de030d070a62ea5c861bb89
github username <github email address>
2019-06-12 09:37:33 -0500

    Set up user reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import search from './searchReducer'
+import user from './userReducer'
 import user_homes from './userHomesReducer'
 import user_favorites from './userFavoritesReducer'
 
 const rootReducer = combineReducers({
   search,
+  user,
   user_homes,
   user_favorites
 });

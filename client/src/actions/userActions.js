1350c036050070f870557703b96e95477bd1c90d
github username <github email address>
2019-06-12 10:04:03 -0500

    Begin moving asyn login action to userActions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,42 @@
+const baseUrl = 'http://localhost:3000';
+
 export const setCurrentUser = user => {
   return {
     type: "SET_CURRENT_USER",
     user
   }
 }
+
+// asynchronous action creators
+export const loginUser = data => {
+  return dispatch => {
+    return fetch(baseUrl + '/login', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+
+
+
+
+
+
+  }
+
+}
+
+
+
+// .then(response => response.json())
+// .then(data => {
+//   if(data.success) {
+//     localStorage.setItem("token", data.token);
+//     this.setState({ error: "" });
+//   } else {
+//     this.setState({ error: "Invalid username or password" })
+//   }
+//   // this.props.login();
+// });

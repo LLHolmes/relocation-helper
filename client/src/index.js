113295b4f6a91ea6e14be6d4fa02a5d641f1f67a
github username <github email address>
2019-06-12 09:08:22 -0500

    Fix Provider typo and comment out serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { thunk } from 'react-thunk';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose; 
-let store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={ store } />
+  <Provider store={ store }>
     <App />
-  </Provider>
-  , document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();

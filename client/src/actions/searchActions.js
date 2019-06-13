6648ed044e47c9f20f9b37e764a0a4b5f33c80e9
github username <github email address>
2019-06-12 22:50:46 -0500

    Zillow search from search form submit successful

--- client/src/actions/searchActions.js
+++ client/src/actions/searchActions.js
@@ -1,1 +1,71 @@
-const API_ZWSID = process.env.REACT_APP_ZWSID;
+import { resetSearchForm } from './formSearchActions.js';
+
+const ZWSID = process.env.REACT_APP_ZWSID;
+
+const zillowSearchBase = `https://cors-anywhere.herokuapp.com/http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=${ZWSID}`
+
+export const setSearch = data => {
+  return {
+    type: "SEARCH_SUCCESS",
+    data
+  }
+}
+
+
+// asynchronous action creators
+export const submitSearch = search => {
+
+  let searchedHome = {
+    search,
+    zpid: '',
+    link: '',
+    latitude: '',
+    longitude: '',
+    assessmentYear: '',
+    assessment: '',
+    yearBuilt: '',
+    lotSize: '',
+    sqFt: '',
+    bathrooms: '',
+    bedrooms: '',
+    lastSoldDate: '',
+    lastSoldPrice: '',
+    zestimate: ''
+  }
+
+  if (search.street) {
+    let address = encodeURIComponent(search.street);
+    let citystatezip = search.zipcode ? encodeURIComponent(search.zipcode) : encodeURIComponent(search.cityState);
+
+    return dispatch => {
+      return fetch(zillowSearchBase + `&address=${address}&citystatezip=${citystatezip}`)
+      .then(response => response.text())
+      .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+      .then(xml => {
+        searchedHome.zpid = xml.getElementsByTagName("zpid")[0].innerHTML;
+        searchedHome.link = xml.getElementsByTagName("homedetails")[0].innerHTML;
+        searchedHome.latitude = xml.getElementsByTagName("latitude")[0].innerHTML;
+        searchedHome.longitude = xml.getElementsByTagName("longitude")[0].innerHTML;
+        searchedHome.assessmentYear = xml.getElementsByTagName("taxAssessmentYear")[0].innerHTML;
+        searchedHome.assessment = xml.getElementsByTagName("taxAssessment")[0].innerHTML;
+        searchedHome.yearBuilt = xml.getElementsByTagName("yearBuilt")[0].innerHTML;
+        searchedHome.lotSize = xml.getElementsByTagName("lotSizeSqFt")[0].innerHTML;
+        searchedHome.sqFt = xml.getElementsByTagName("finishedSqFt")[0].innerHTML;
+        searchedHome.bathrooms = xml.getElementsByTagName("bathrooms")[0].innerHTML;
+        searchedHome.bedrooms = xml.getElementsByTagName("bedrooms")[0].innerHTML;
+        searchedHome.lastSoldDate = xml.getElementsByTagName("lastSoldDate")[0].innerHTML;
+        searchedHome.lastSoldPrice = xml.getElementsByTagName("lastSoldPrice")[0].innerHTML;
+        searchedHome.zestimate = xml.getElementsByTagName("amount")[0].innerHTML;
+
+        dispatch(setSearch(searchedHome))
+        dispatch(resetSearchForm())
+      })
+      .catch(alert("Something went wrong. Please try again."))
+    };
+  } else {
+    alert("Please fill out your street address.");
+    return dispatch => {
+      dispatch(resetSearchForm())
+    };
+  };
+}
--- client/src/reducers/searchReducer.js
+++ client/src/reducers/searchReducer.js
@@ -1,6 +1,11 @@
-export default (state = { homes: [], loading: false }, action) => {
+export default (state = { search: {}, comps: {} }, action) => {
   switch (action.type){
+    case "SEARCH_SUCCESS":
+      return {
+        ...state,
+        search: action.data
+      };
     default:
-    return state;
+      return state;
   };
 };

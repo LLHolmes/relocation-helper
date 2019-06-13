e9846d5d5edd9b3969cd43482290333e17194c82
github username <github email address>
2019-06-13 15:07:01 -0500

    Build out UserHomeCard HTML

--- client/src/components/UserHomeCard.js
+++ client/src/components/UserHomeCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const UserHomeCard = ({ home }) => {
-  console.log(home)
   return (
-    <div className="UserHomeCard">
-      Individual Home
-    </div>
+    <fieldset className="UserHomeCard">
+      <legend><h1 className="columnClear">{ home.search.street }, { home.search.cityState } { home.search.zipcode}</h1></legend>
+      <div className="col3">
+        <p className="column">{ home.bedrooms } bed/{ home.bathrooms } bath</p>
+        <p className="column">{ home.sqFt } sqFt., { home.lotSize } lot</p>
+        <p className="column">Built: { home.yearBuilt }</p>
+      </div>
+      <h3>Estimated value: ${ home.zestimate }</h3>
+    </fieldset>
   );
 }
 

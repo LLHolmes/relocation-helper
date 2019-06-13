d5ab7225c55f2a3990fc536894381616a1d5deab
github username <github email address>
2019-06-13 13:49:30 -0500

    User homes search on componentDidMount working

--- client/src/reducers/userHomesReducer.js
+++ client/src/reducers/userHomesReducer.js
@@ -1,6 +1,11 @@
 export default (state = [], action) => {
   switch (action.type){
+    case "SEARCH_SUCCESS":
+      return [
+        ...state,
+        action.data
+      ];
     default:
-    return state;
+     return state;
   };
 };

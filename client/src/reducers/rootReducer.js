ad6d0ad1dddc82c87615518a856275d9d95a56c5
github username <github email address>
2019-06-13 12:11:21 -0500

    Comment out unused userFavoritesReducer and add storeInitialState for reference

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import search from './searchReducer'
 import user from './userReducer'
 import userHomes from './userHomesReducer'
-import userFavorites from './userFavoritesReducer'
+// import userFavorites from './userFavoritesReducer'
 import formLogin from './formLoginReducer'
 import formSignup from './formSignupReducer'
 import formSearch from './formSearchReducer'
@@ -11,10 +11,20 @@ const rootReducer = combineReducers({
   search,
   user,
   userHomes,
-  userFavorites,
+  // userFavorites,
   formSignup,
   formLogin,
   formSearch
 });
 
 export default rootReducer;
+
+// FOR REFERENCE:
+// storeInitialState = {
+//   search: { search: {}, comps: {} },
+//   user: null,
+//   userHomes: [],
+//   formSignup: { name: '', email: '', password: '' }
+//   formLogin: { email: '', password: '' }
+//   formSearch: { street: '', cityState: '', zipcode: '' }
+// }

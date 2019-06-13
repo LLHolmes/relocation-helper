c3864c9839bfce1c565d3c37ac802931cf74349f
github username <github email address>
2019-06-12 20:54:14 -0500

    Add RESET_SEARCH_FORM to clear search form to initial state

--- client/src/reducers/formSearchReducer.js
+++ client/src/reducers/formSearchReducer.js
@@ -8,6 +8,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_SEARCH_FORM":
       return action.formData;
+    case "RESET_SEARCH_FORM":
+      return initialState;
     default:
       return state;
   };

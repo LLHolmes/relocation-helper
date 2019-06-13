86a29fe8a28d88efa2b4f03606c36c0a7c45afc5
github username <github email address>
2019-06-12 19:44:20 -0500

    Clean up Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { updateLoginForm } from '../actions/formLoginActions.js'
 import { loginUser } from '../actions/userActions.js'
 
-// const baseUrl = "http://localhost:3000";
-
 const Login = ({ formLogin, updateLoginForm, loginUser }) => {
 
   const handleChange = event => {
@@ -15,44 +13,7 @@ const Login = ({ formLogin, updateLoginForm, loginUser }) => {
     };
     updateLoginForm(updatedFormInfo)
   };
-
-  // state = {
-  //   email: '',
-  //   password: '',
-  //   error: ''
-  // };
-
-  // handleChange = event => {
-  //   this.setState({ [event.target.name]: event.target.value });
-  // };
-  //
-  // handleLogin = event => {
-  //   event.preventDefault();
-  //
-  //   let params = {
-  //     email: this.state.email,
-  //     password: this.state.password
-  //   };
-  //
-  //   fetch(baseUrl + '/login', {
-  //     method: "POST",
-  //     headers: {
-  //       "content-type": "application/json",
-  //       Accept: "application/json"
-  //     },
-  //     body: JSON.stringify(params)
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     if(data.success) {
-  //       localStorage.setItem("token", data.token);
-  //       this.setState({ error: "" });
-  //     } else {
-  //       this.setState({ error: "Invalid username or password" })
-  //     }
-  //     // this.props.login();
-  //   });
-  // };
+  
   const handleSubmit = event => {
     event.preventDefault();
     loginUser(formLogin)

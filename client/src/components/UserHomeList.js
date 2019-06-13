15d46f33c194da93731cadefd1e4233dc1775bcb
github username <github email address>
2019-06-13 13:50:23 -0500

    Build working UserHomeList Component

---
+++ client/src/components/UserHomeList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { searchUserHomes } from '../actions/userHomesActions.js';
+import UserHomeCard from './UserHomeCard.js';
+
+class UserHomeList extends Component {
+
+  componentDidMount() {
+    this.props.homes.forEach(home => {
+      this.props.searchUserHomes(home);
+    })
+  };
+
+  render() {
+    const showUserHomes = this.props.userHomes.map((home, index) => 
+      <UserHomeCard home={home} key={index} />
+    );
+
+    return (
+      <div className="UserHomeList">
+        { showUserHomes }
+      </div>
+    );
+  };
+}
+
+const mapStateToProps = state => {
+  return {
+    homes: state.user.homes,
+    userHomes: state.userHomes
+  }
+}
+
+export default connect(mapStateToProps, { searchUserHomes })(UserHomeList);

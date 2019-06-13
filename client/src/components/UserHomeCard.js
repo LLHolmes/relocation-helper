d748b683ada20b64e033444da298a0f258e5a38a
github username <github email address>
2019-06-13 13:51:32 -0500

    Create basic UserHomeCard, need to build return out

---
+++ client/src/components/UserHomeCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserHomeCard = props => {
+  // console.log(props)
+  return (
+    <div className="UserHomeCard">
+      Individual Home
+    </div>
+  );
+}
+
+export default UserHomeCard;

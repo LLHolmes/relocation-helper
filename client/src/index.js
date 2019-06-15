da899b014c20c2b500b30fddbc6f93203a39ddbd
github username <github email address>
2019-06-15 11:39:45 -0500

    Add react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './store'
 // import * as serviceWorker from './serviceWorker';
 import './stylesheets/index.css';
 
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 

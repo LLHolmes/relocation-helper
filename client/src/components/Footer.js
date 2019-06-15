03030eae81a7d53fa68aa8af16ff0b9cfb7d2feb
github username <github email address>
2019-06-14 21:26:23 -0500

    Add Zillow required logo and disclaimers

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <div className="Footer">
+      <img src="http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif" alt="Zillow Logo" height="50" width="200"/>
+
+       © Zillow, Inc., 2006-2016. Use is subject to <a className="zillow" href="https://www.zillow.com/corp/Terms.htm">Terms of Use </a> - 
+      <a className="zillow" href="https://www.zillow.com/wikipages/What-is-a-Zestimate/">What's a Zestimate?</a>
+    </div>
+  );
+}
+
+export default Footer;

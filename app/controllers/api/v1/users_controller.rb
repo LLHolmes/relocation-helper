d86baf723bb2c01b2d0131d70f1f6761c2b91f8c
github username <github email address>
2019-06-16 13:17:10 -0500

    Clean up file to remove unused route methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::UsersController < ApplicationController
 
-  # def show_user
-  #   render json: current_user
-  #   # { name: current_user.name, email: current_user.email }
-  # end
-
   def create
     @user = User.new(user_params)
     if @user.save

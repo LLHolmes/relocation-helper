b36858cf5e7f674683ba3c8af581c88e7accf3b2
github username <github email address>
2019-06-15 21:19:47 -0500

    Add react-router NavLinks

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Login from './Login';
+import { NavLink } from 'react-router-dom';
+// import Login from './Login';
 // import Signup from './Signup';
 import Logout from './Logout';
 
@@ -8,8 +9,17 @@ const NavBar = ({ user }) => {
 
   return (
     <div className="NavBar">
-      <strong>{ user ? `Welcome ${user.name}` : ''}</strong>
-      { user ? <Logout /> : <><Login />SUB</>}
+      <NavLink className="NavLeft" to="/">Home</NavLink>
+      { user ? (
+        <NavLink className="NavUser" to="/member/homes">Welcome {user.name}</NavLink>
+      ) : (
+        <NavLink className="NavRight" to="/login">Login</NavLink>
+      )}
+      { user ? (
+        <Logout />
+      ) : (
+        <NavLink className="NavRight" to="/signup">Signup</NavLink>
+      )}
     </div>
   );
 }

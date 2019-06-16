14e5be3df5aac7121bc2bad1c5f08a376685e3e4
github username <github email address>
2019-06-16 13:11:31 -0500

    Remove mentions of unused userFavoritesReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import search from './searchReducer'
 import user from './userReducer'
 import userHomes from './userHomesReducer'
-// import userFavorites from './userFavoritesReducer'
 import formLogin from './formLoginReducer'
 import formSignup from './formSignupReducer'
 import formSearch from './formSearchReducer'
@@ -11,7 +10,6 @@ const rootReducer = combineReducers({
   search,
   user,
   userHomes,
-  // userFavorites,
   formSignup,
   formLogin,
   formSearch

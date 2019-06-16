f2553f36fab97009715cd21f20b9f3609837e281
github username <github email address>
2019-06-16 13:13:17 -0500

    Clean up file to remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,10 @@ Rails.application.routes.draw do
   post '/login', to: 'auth#create'
   delete '/logout', to: 'auth#destroy'
   get '/get_current_user', to: 'auth#get_current_user'
-  # get '/user', to: 'users#show_user'
-  # # resources :users
-  # resources :homes, except: [:new, :edit]
-  # resources :favorites, except: [:new, :edit, :update]
-
-  # http://localhost:3000/api/v1/resource
+  
   namespace :api do
     namespace :v1 do
-      # get '/user', to: 'users#show_user'
       resources :homes, only: [:create, :destroy]
-      # resources :favorites, except: [:new, :edit, :update]
       post '/signup', to: 'users#create'
       delete '/unsubscribe', to: 'users#destroy'
     end

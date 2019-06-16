861aa7ee4d87a9a06a3696086fdcbfd8ab21bc97
github username <github email address>
2019-06-15 21:29:00 -0500

    Add react-router-dom and conditional statement for switch, routes, and redirects based on if a user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import { getCurrentUser } from './actions/userActions.js'
 import './stylesheets/App.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer.js';
 import MainContainer from './components/MainContainer';
-// import Signup from './components/Signup';
+import Signup from './components/Signup';
+import Login from './components/Login';
 // import Unsubscribe from './components/Unsubscribe';
-// import UserPage from './components/UserPage';
+import MemberShow from './components/MemberShow';
 // import HomeSearchForm from './components/HomeSearchForm';
 
 class App extends Component {
@@ -18,11 +22,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <div className="Nav">
           <NavBar />
         </div>
-        <div>
-          <MainContainer />
+        <div className="MainBody">
+          { this.props.user ? (
+              <Switch>
+                <Route exact path ="/member/homes" component={ MemberShow } />
+                <Route component={ MainContainer } />
+                <Redirect from="/signup" to="/"/>
+                <Redirect from="/login" to="/"/>
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path ="/signup" component={ Signup } />
+                <Route exact path ="/login" component={ Login } />
+                <Route component={ MainContainer } />
+                <Redirect from="/member/homes" to="/login"/>
+              </Switch>
+            )
+          }
+        </div>
+        <div className="Foot">
           <Footer />
         </div>
       </div>
@@ -30,4 +51,33 @@ class App extends Component {
   };
 };
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = ({ user }) => {
+  return {
+    user
+  };
+};
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
+
+// <Route exact path ="/signup" component={ Signup } />
+// <Route exact path ="/login" component={ Login } />
+// { user ? <Route exact path ="/member/homes" component={ MemberShow } /> : <Redirect from="/member/homes" to="/login"}
+// <Route component={ MainContainer } />
+
+// { user =>
+//   user ? (
+//     <Switch>
+//       <Route exact path ="/signup" component={ Signup } />
+//       <Route exact path ="/login" component={ Login } />
+//       <Route exact path ="/member/homes" component={ MemberShow } />
+//       <Route component={ MainContainer } />
+//     </Switch>
+//   ) : (
+//     <Switch>
+//       <Route exact path ="/signup" component={ Signup } />
+//       <Route exact path ="/login" component={ Login } />
+//       <Route component={ MainContainer } />
+//       <Redirect from="/member/homes" to="/login"/>
+//     </Switch>
+//   )
+// }

ef9f153320af662b65a97862e6f2e3ca46e07d5a
github username <github email address>
2019-06-16 13:14:46 -0500

    Clean up file to remove Rails version references

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,19 +1,11 @@
 class AuthController < ApplicationController
-  # skip_before_action :authenticate_user, only: [:create]
 
   def create
     user = User.find_by(email: params[:auth][:email])
     if user && user.authenticate(params[:auth][:password])
-      # Rails version:
       session[:user_id] = user.id
       render json: user
-
-      # token = encode({ user_id: user.id })
-      # render json: { token: token, success: true }
     else
-      # Rails version:
-      # indicate some kind of error
-      # keep errors generic for safety (not e-mail or password wrong, just something went wrong)
       render json: { error: "Invalid Login" }, status: 401
     end
   end

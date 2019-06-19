c9a8162762cc156bc0ac58c8dced242dad22f29f
github username <github email address>
2019-06-18 19:00:24 -0500

    Add proper punctuation

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className="Footer">
       <img src="http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif" alt="Zillow Logo" height="50" width="200"/>
 
-       © Zillow, Inc., 2006-2016. Use is subject to <a className="zillow" target="_blank" rel="noopener noreferrer" href="https://www.zillow.com/corp/Terms.htm">Terms of Use </a> - 
+      © Zillow, Inc., 2006-2016. Use is subject to <a className="zillow" target="_blank" rel="noopener noreferrer" href="https://www.zillow.com/corp/Terms.htm">Terms of Use </a> -
       <a className="zillow" target="_blank" rel="noopener noreferrer" href="https://www.zillow.com/wikipages/What-is-a-Zestimate/">What's a Zestimate?</a>
     </div>
   );
-}
+};
 
 export default Footer;

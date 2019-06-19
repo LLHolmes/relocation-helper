c5716c8530f76e91f93dc1bf83922b1abdd498c1
github username <github email address>
2019-06-18 19:02:59 -0500

    Add padding to right of Zillow logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 
   return (
     <div className="Footer">
-      <img src="http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif" alt="Zillow Logo" height="50" width="200"/>
+      <img className="Zillowlogo" src="http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif" alt="Zillow Logo" height="50" width="200"/>
 
       © Zillow, Inc., 2006-2016. Use is subject to <a className="zillow" target="_blank" rel="noopener noreferrer" href="https://www.zillow.com/corp/Terms.htm">Terms of Use </a> -
       <a className="zillow" target="_blank" rel="noopener noreferrer" href="https://www.zillow.com/wikipages/What-is-a-Zestimate/">What's a Zestimate?</a>

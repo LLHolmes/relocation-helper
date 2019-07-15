bc601326c873a26d433c6be79bc7b3396feab921
LLHolmes <lana.lee.holmes@gmail.com>
2019-07-15 09:11:10 -0500

    Add routes to search homes and comps from Zillow API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   post '/login', to: 'auth#create'
   delete '/logout', to: 'auth#destroy'
   get '/get_current_user', to: 'auth#get_current_user'
-  
+
+  get '/search_home', to 'zillow#search_home'
+  get '/search_comps', to 'zillow#search_comps'
+
   namespace :api do
     namespace :v1 do
       resources :homes, only: [:create, :destroy]

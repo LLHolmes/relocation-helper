0d0b5400acec6b8e9f32af58c5fadc70d682493c
LLHolmes <lana.lee.holmes@gmail.com>
2019-07-15 15:55:10 -0500

    Implement search_comps Faraday call

--- app/controllers/zillow_controller.rb
+++ app/controllers/zillow_controller.rb
@@ -1,9 +1,5 @@
 class ZillowController < ApplicationController
-  # ZWSID = ENV['ZILLOW_ZWSID']
-  #
-  # zillowSearchBase = `https://cors-anywhere.herokuapp.com/http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=${ZWSID}`
-  # zillowCompsBase = `https://cors-anywhere.herokuapp.com/http://www.zillow.com/webservice/GetDeepComps.htm?zws-id=${ZWSID}`
-
+  
   def search_home
     @response = Faraday.get('http://www.zillow.com/webservice/GetDeepSearchResults.htm?') do |req|
       req.params['zws-id'] = ENV['ZILLOW_ZWSID']
@@ -19,8 +15,6 @@ class ZillowController < ApplicationController
         @data = @body['SearchResults:searchresults']['response']['results']['result']
 
         @searchedHome = {
-          # apiId: '',   ///FOR USERHOMES ONLY ///
-          # score: '',   /// FOR COMPS ONLY ///
           zpid: @data['zpid'],
           link: @data['links']['homedetails'],
           street: @data['address']['street'],
@@ -57,6 +51,52 @@ class ZillowController < ApplicationController
   end
 
   def search_comps
+    @response = Faraday.get('http://www.zillow.com/webservice/GetDeepComps.htm?') do |req|
+      req.params['zws-id'] = ENV['ZILLOW_ZWSID']
+      req.params['zpid'] = params[:zpid]
+      req.params['count'] = 15
+    end
+
+    if @response
+      @body = Crack::XML.parse(@response.body)
+      @message = @body['Comps:comps']['message']['code']
+
+      if @message == '0'
+        @comps_data = @body['Comps:comps']['response']['properties']['comparables']['comp']
+
+        @comps = @comps_data.map { |data|
+          comp = {
+            score: data['score'].to_f,
+            zpid: data['zpid'],
+            link: data['links']['homedetails'],
+            street: data['address']['street'],
+            zipcode: data['address']['zipcode'],
+            city: data['address']['city'],
+            state: data['address']['state'],
+            latitude: data['address']['latitude'],
+            longitude: data['address']['longitude'],
+            assessmentYear: data['taxAssessmentYear'],
+            assessment: data['taxAssessment'],
+            yearBuilt: data['yearBuilt'],
+            lotSize: data['lotSizeSqFt'],
+            sqFt: data['finishedSqFt'],
+            bathrooms: data['bathrooms'],
+            bedrooms: data['bedrooms'],
+            lastSoldDate: data['lastSoldDate'],
+            lastSoldPrice: data['lastSoldPrice'],
+            zestimate: data['zestimate']['amount']
+          }
+        }.sort! {|a, b| b[:score] <=> a[:score]}
+
+        render json: @comps, status: 200
+      elsif @message == '3' || @message == '4'
+        render json: { error: 'The Zillow Web Service is currently not available. Please come back later and try again.' }, status: 503
+      elsif @message == '504'
+        render json: { error: 'Sorry, there were no comparables for the property you specified.' }, status: 502
+      else
+        render json: { error: 'Something went wrong. Please try again.' }, status: 501
+      end
+    end
   end
 
 

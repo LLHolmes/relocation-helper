f836ac04bd9cb2341573454044b246c1834f1926
LLHolmes <lana.lee.holmes@gmail.com>
2019-08-24 12:27:08 -0400

    Create userFavoritesReducer.js

---
+++ client/src/reducers/userFavoritesReducer.js
@@ -0,0 +1,20 @@
+export default (state = [], action) => {
+  switch (action.type){
+    case "SEARCH_USERFAVORITE_SUCCESS":
+      return [
+        ...state,
+        action.data
+      ];
+    case "ADD_SAVED_USERFAVORITE":
+      return [
+        ...state,
+        action.data
+      ];
+    case "REMOVE_USER_SEARCHED_FAVORITE":
+      return state.filter(favorite => favorite.apiId !== action.id);
+    case "CLEAR_USER_FAVORITES":
+      return [];
+    default:
+     return state;
+  };
+};

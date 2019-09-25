2a885c52a10b25f3ebe940c2490215ab58549a4f
LLHolmes <lana.lee.holmes@gmail.com>
2019-09-25 16:16:35 -0400

    Rename CompMap class and add defaultProps

--- client/src/components/CompMap.js
+++ client/src/components/CompMap.js
@@ -4,21 +4,33 @@ import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const MainContainer = ({ comps }) => {
+// const CompMap = ({ comps }) => {
+class CompMap extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
-  return (
-    <div className="CompMap" style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_GOOGLE_MAP_API }}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+  render() {
+    return (
+      <div className="CompMap" style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_GOOGLE_MAP_API }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
@@ -27,4 +39,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(MainContainer);
+export default connect(mapStateToProps)(CompMap);

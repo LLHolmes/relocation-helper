5d3738179c374730908dbb49cd96add41d664cb6
LLHolmes <lana.lee.holmes@gmail.com>
2019-09-25 16:10:42 -0400

    Add CompMap.js to CompList.js render

--- client/src/containers/CompList.js
+++ client/src/containers/CompList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeComp } from '../actions/searchActions.js';
 import CompHomeCard from '../components/CompHomeCard.js';
+import CompMap from '../components/CompMap.js';
 
 class CompList extends Component {
 
@@ -15,6 +16,7 @@ class CompList extends Component {
 
     return (
       <div className="CompList">
+        <CompMap />
         { comps.length > 0 ? <h3  className="cardHolder">Comparable Homes to {search.street}, {search.city}, {search.state} {search.zipcode} </h3> : <h3>Enter a search or select a saved home to recieve comparable home information</h3>}
         { showComps }
         { comps.length > 0 ? <Link to='/comps/index'>Print-friendly List</Link> : ''}

50b27e45b1ccc978abfd7452de194b38be40e128
LLHolmes <lana.lee.holmes@gmail.com>
2019-09-30 13:03:55 -0400

    Change REACT_APP_GOOGLE_MAP_API name so process.env works

--- client/src/components/CompMap.js
+++ client/src/components/CompMap.js
@@ -18,7 +18,7 @@ class CompMap extends React.Component {
     return (
       <div className="CompMap" style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_GOOGLE_MAP_API }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
